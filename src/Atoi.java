6ec7c138912a3798010553bc3e146563e1a859b1
frank <630656007@qq.com>
2021-03-14 16:45:12 +0800

    oj2 oj3

--- src/Atoi.java
+++ src/Atoi.java
@@ -5,7 +5,7 @@ public class Atoi {
    */
   final static int INT_MAX = 2147483647;
   final static int INT_MIN = -2147483648;
-  public int atoi(String str) {
+  public Object atoi(String str) {
     int sign = 1;
     if(str.charAt(0) == '-'){
       str = str.substring(1);
