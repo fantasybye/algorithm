4d537b42268a7a2ba6180b4a44b3c96637789a92
ffffrank <630656007@qq.com>
2021-03-14 22:06:49 +0800

    oj

--- src/InsertSortSolution.java
+++ src/InsertSortSolution.java
@@ -7,21 +7,43 @@ public class InsertSortSolution {
         }
         for (int i = 1;i<arr.length;i++){
             for(int j = i - 1;j>=0&&arr[j]>arr[j+1];j--){
-                Util.swap(arr,j,j+1);
+                swap(arr,j,j+1);
             }
         }
     }
 
+    public static void swap(int[] arr,int i, int j){
+        arr[i] = arr[i]^arr[j];
+        arr[j] = arr[i]^arr[j];
+        arr[i] = arr[i]^arr[j];
+    }
 
+    public static void displayArray(int[] arr){
+        for(int i = 0; i < arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.print(arr[arr.length-1]);
+        System.out.println();
+    }
+
+    public static int[] handleInput(Scanner s){
+        String str = s.nextLine();
+        String[] sarr = str.split(" ");
+        int[] arr = new int[sarr.length-1];
+        for(int i = 0;i < arr.length; i++){
+            arr[i] = Integer.parseInt(sarr[i+1]);
+        }
+        return arr;
+    }
 
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int ucCount = s.nextInt();
         s.nextLine();
         while (ucCount-->0){
-            int[] arr = Util.handleInput(s);
+            int[] arr = handleInput(s);
             insertSort(arr);
-            Util.displayArray(arr);
+            displayArray(arr);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,58 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      StrStr solution = new StrStr();
-      System.out.print(solution.strStr("source", "rced"));
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            int T = sc.nextInt();
+            while (T-- > 0) {
+                int size = sc.nextInt();
+                int[] arr = new int[size];
+                for(int i = 0;i < size; i++){
+                    arr[i] = sc.nextInt();
+                }
+                layerOrder(arr);
+            }
+        }
+    }
+    public static void layerOrder(int[] arr){
+        Queue<Integer> queue = new LinkedList<>();
+        if(arr==null) {System.out.println();return;}
+        queue.offer(0);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            int[] res = new int[size];
+            for(int i = 0;i<size;i++){
+                int index = queue.poll();
+                if(index * 2 + 1 <= arr.length - 1)
+                    queue.offer((2*index + 1));
+                if(index * 2 + 2 <=arr.length - 1)
+                    queue.offer((2*index +2));
+                res[i] = arr[index];
+            }
+            Arrays.sort(res);
+            println(res);
+        }
+
+    }
+    private static void println(int[] arr) {
+        StringBuilder sb = getStringBuilder(arr);
+        System.out.println(sb.toString());
+    }
+
+    private static StringBuilder getStringBuilder(int[] arr) {
+        int n = arr.length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]);
+            if (i != n - 1) {
+                sb.append(" ");
+            }
+        }
+        return sb;
     }
 }
+
