a336b39696bc3f63102274908e8713eb0036e499
ffffrank <630656007@qq.com>
2021-03-18 20:08:10 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,103 +1,45 @@
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
-
-    private static int T;
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        T = in.nextInt();
-        in.nextLine();
-        while (T-- != 0) {
-            String a = in.nextLine();
-            String b = in.nextLine();
-
-            Set<String> lcsSet = LCS(a, b);
-
-            ArrayList<String> list = new ArrayList<>(lcsSet);
-            if (list.size() == 1 && list.get(0).equals("")){
-                continue;
+        Scanner s = new Scanner(System.in);
+        int count = s.nextInt();
+        s.nextLine();
+        while(count-->0) {
+            int n = s.nextInt();
+            int m = s.nextInt();
+            int[][] arr = new int[n][m];
+            for(int i = 0;i < n;i++){
+                for (int j = 0;j < m;j++){
+                    arr[i][j] = s.nextInt();
+                }
             }
-            list.sort(String::compareTo);
-            for (int i = 0; i < list.size(); i++) {
-                print(list.get(i), list.size()-1-i);
-            }
+            System.out.println(getMaxMatrix(arr));
         }
     }
 
-    private static void print(String s, int n){
-        if (T == 0 && n == 0){
-            System.out.println(s);
-        }else{
-            System.out.println(s);
-        }
-    }
-
-    public static Set<String> LCS(String a, String b) {
-        int n = a.length(), m = b.length();
-        //c[i][j]表示a长度为i和b长度为j时的最长公共子序列长度
-        int[][] c = new int[n+1][m+1];
-        //d[i][j]表示方向
-        char[][] d = new char[n+1][m+1];
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                if(a.charAt(i-1) == b.charAt(j-1)) {
-                    c[i][j] = c[i-1][j-1] + 1;
-                    //左上
-                    d[i][j] = '↖';
-                } else if(c[i-1][j] > c[i][j-1]){
-                    c[i][j] = c[i-1][j];
-                    //上
-                    d[i][j] = '↑';
-                } else if(c[i-1][j] < c[i][j-1]){
-                    c[i][j] = c[i][j-1];
-                    //左
-                    d[i][j] = '←';
-                } else {
-                    c[i][j] = c[i][j-1];
-                    //可向左可向右
-                    d[i][j] = '┘';
+    public static int getMaxMatrix(int[][] matrix) {
+        int max=Integer.MIN_VALUE;
+        int dp=0,start=0;
+        int ans=0;//结果
+        int[] sum=null;//纵向累加数组
+        for(int i=0;i<matrix.length;i++) {
+            sum=new int[matrix[0].length];
+            for(int j=i;j<matrix.length;j++) {
+                dp=0;start=0;
+                for(int k=0;k<sum.length;k++) {
+                    sum[k]+=matrix[j][k];
+                    dp+=sum[k];
+                    if(max<dp) {
+                        ans = (j-i)*(k-start);
+                        max=dp;
+                    }
+                    if(dp<0) {
+                        dp=0;start=k+1;
+                    }
                 }
             }
         }
-        String lcs = "";
-        Set<String> lcsSet = new HashSet<>();
-        backTrace(d,a,lcs,n,m,c[n][m],lcsSet);
-
-        return lcsSet;
+        return ans;
     }
-
-    private static void backTrace(char[][] d, String a, String lcs, int i , int j, int maxSublen, Set<String> lcsSet){
-        if(i == 0 || j == 0) {
-            StringBuilder sb = new StringBuilder(lcs);
-            lcs = sb.reverse().toString();
-            //可能有些提早出来了，一定要判断长度是最长的，但是这样还是会有重复的字符串，所以还要做去重处理
-            if (lcs.length() == maxSublen) {
-                lcsSet.add(lcs);
-            }
-            return;
-        }
-
-        switch (d[i][j]){
-            case '↖':
-                lcs += a.charAt(i-1);
-                backTrace(d,a,lcs,i-1,j-1,maxSublen,lcsSet);
-                break;
-            case '↑':
-                backTrace(d,a,lcs,i-1,j,maxSublen,lcsSet);
-                break;
-            case '←':
-                backTrace(d,a,lcs,i,j-1,maxSublen,lcsSet);
-                break;
-            case '┘':
-                backTrace(d,a,lcs,i-1,j,maxSublen,lcsSet);
-                backTrace(d,a,lcs,i,j-1,maxSublen,lcsSet);
-                break;
-        }
-    }
-
 }
