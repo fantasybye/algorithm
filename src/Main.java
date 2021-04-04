a77a97ead58b4482231f0d6011c6a6d25825bb32
ffffrank <630656007@qq.com>
2021-04-04 12:11:10 +0800

    oj4

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,30 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int numOftest = scanner.nextInt();
-        while (numOftest-->0) {
-            int N = scanner.nextInt();
-            int[] x = new int[N];
-            int[] y = new int[N];
-            for(int i = 0;i<N;i++){
-                x[i] = scanner.nextInt();
-                y[i] = scanner.nextInt();
+        Scanner s = new Scanner(System.in);
+        int count = s.nextInt();
+        s.nextLine();
+        while(count-->0){
+            int[] arr = handleInput(s);
+            int[] interval = handleInput(s);
+            int left = interval[0],right = interval[1];
+            int[] narr = new int[right-left+1];
+            for(int i = 0;i < narr.length;i++){
+                narr[i] = arr[left-1+i];
             }
-            func(N, x, y);
+            Arrays.sort(narr);
+            int k = Integer.parseInt(s.nextLine());
+            System.out.println(narr[k-1]);
         }
     }
-    static class Position{
-        int x;
-        int y;
-        Position(int x, int y){
-            this.x = x;
-            this.y = y;
+    public static int[] handleInput(Scanner s){
+        String str = s.nextLine();
+        String[] sarr = str.split(" ");
+        int[] arr = new int[sarr.length];
+        for(int i = 0;i < arr.length; i++){
+            arr[i] = Integer.parseInt(sarr[i]);
         }
-    }
-
-    private static void func(int n, int[] x, int[] y){
-        Map<Integer, Integer> xmap = new HashMap<>();
-        Map<Integer, Integer> ymap = new HashMap<>();
-        Map<Position, Integer> xymap = new HashMap<>();
-
-        for(int i = 0 ; i < n ; i ++){
-            int countx = xmap.getOrDefault(x[i], 0);
-            countx++;
-            xmap.put(x[i], countx);
-
-            int county = ymap.getOrDefault(y[i], 0);
-            county++;
-            ymap.put(y[i], county);
-
-            Position position = new Position(x[i], y[i]);
-            int countxy = xymap.getOrDefault(position, 0);
-            countxy ++;
-            xymap.put(position, countxy);
-        }
-
-        int res = 0;
-        for (Integer i: xmap.values()){
-            res += i*(i-1)/2;
-        }
-        for(Integer i : ymap.values()){
-            res += i*(i-1)/2;
-        }
-        for(Integer i : xymap.values()){
-            res -= i*(i-1)/2;
-        }
-        System.out.println(res);
+        return arr;
     }
 }
